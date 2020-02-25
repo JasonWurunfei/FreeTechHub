fc13aa84be5c27d54b96de1881e530a792d2942d
ELAINEYangyin <2515997297@qq.com>
2020-02-25 23:29:34 +0800

    register which need real email

--- FreeTechHub/home/tokens.py
+++ FreeTechHub/home/tokens.py
@@ -4,8 +4,8 @@ from six import text_type
 class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return (
-            six.text_type(user.pk) + six.text_type(timestamp) +
-            six.text_type(user.profile.signup_confirmation)
+            text_type(user.pk) + text_type(timestamp) +
+            text_type(user.profile.signup_confirmation)
         )
 
 account_activation_token = AccountActivationTokenGenerator()
--- FreeTechHub/home/views.py
+++ FreeTechHub/home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
+#from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm
@@ -16,6 +16,8 @@ from django.utils.http import urlsafe_base64_encode
 from .tokens import account_activation_token
 from django.template.loader import render_to_string
 from .tokens import account_activation_token
+from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
 def index(request):
     return render(request, 'home/index.html')
@@ -36,52 +38,39 @@ def activate(request, uidb64, token):
         # set signup_confirmation true
         user.profile.signup_confirmation = True
         user.save()
-        login(request, user)
-        return redirect('home/login.html')
+        login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+        return HttpResponseRedirect(reverse('home:index'))
     else:
         return render(request, 'home/activation_invalid.html')
+
+@csrf_exempt
 def register(request):
-    form = SignUpForm(request.POST)
-    if request.method  != 'POST':
-       form = SignUpForm()
-    else:
-       form = SignUpForm(data=request.POST)
-       if form.is_valid():
-          user = form.save()
-          user.refresh_from_db()
-          user.profile.first_name = form.cleaned_data.get('username')
-          user.profile.email = form.cleaned_data.get('email')
-          user.is_active = False
-          user.save()
-          current_site = get_current_site(request)
-          subject = 'Please Activate Your Account'
-          message = render_to_string('home/activation_request.html', {
+    if request.method  == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.profile.username = form.cleaned_data.get('username')
+            user.profile.email = form.cleaned_data.get('email')
+            # user can't login until link confirmed
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            subject = 'Please Activate Your Account'
+            # load a template like get_template() 
+            # and calls its render() method immediately.
+            message = render_to_string('home/activation_request.html', {
                 'user': user,
                 'domain': current_site.domain,
                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
                 # method will generate a hash value with user related data
                 'token': account_activation_token.make_token(user),
             })
-          user.email_user(subject, message)
-          return redirect('home/activation_sent')
-    return render(request, 'home/login.html', {'form': form})
-def activate(request, uidb64, token):
-    try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid)
-    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-        user = None
-    # checking if the user exists, if the token is valid.
-    if user is not None and account_activation_token.check_token(user, token):
-        # if valid set active true 
-        user.is_active = True
-        # set signup_confirmation true
-        user.profile.signup_confirmation = True
-        user.save()
-        login(request, user)
-        return redirect('home/login.html')
+            user.email_user(subject, message)
+            return redirect('home:activation_sent')
     else:
-        return render(request, 'home/activation_invalid.html')
+        form = SignUpForm()
+    return render(request, 'home/signup.html', {'form': form})
 
 
 
