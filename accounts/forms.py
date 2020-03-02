07fe3448e6c4983de15382e58a13480b3ff555b4
ELAINEYangyin <2515997297@qq.com>
2020-03-02 22:22:47 +0800

    change the code style

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,9 +7,9 @@ class RegisterForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = [
-		"username",
-		"email", 
-		"password1", 
-		"password2"
+			"username",
+			"email", 
+			"password1", 
+			"password2"
 		]
 		
