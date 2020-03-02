1c76b0581875427ba99ce80a3bb98e0c7247dd8e
ELAINEYangyin <2515997297@qq.com>
2020-03-02 19:09:32 +0800

    change the code style

--- FTH/urls.py
+++ FTH/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/',include('accounts.urls', namespace='accounts')),
-     path('',include('home.urls', namespace='home')),
+	path('admin/', admin.site.urls),
+	path('accounts/',include('accounts.urls', namespace='accounts')),
+	path('',include('home.urls', namespace='home')),
 ]
