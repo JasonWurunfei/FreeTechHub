d069bb59dc983664b13bd6ea5bb5b7d54eda5192
ELAINEYangyin <2515997297@qq.com>
2020-03-03 12:14:14 +0800

    change the code style

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.http import HttpResponseRedirect
+# Form related
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth import login, logout, authenticate
+# Others
 import datetime
-def register(request):
-	if request.method == 'POST':
-		form = RegisterForm(request.POST)
-		if form.is_valid():
-			form.save()
-			return redirect("accounts:login")
-	else:
-		form = RegisterForm(request.POST)
 
-	return render(request, 'registration/register.html', context={'form': form})
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/accounts/login")
+    else:
+        form = RegisterForm(request.POST)
+
+    return render(request, 'registration/register.html', context={'form': form})
+
