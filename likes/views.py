06eff0aa120119ccff80df5d342acc317723c257
YKK-cp <61101565+YKK-cp@users.noreply.github.com>
2020-03-04 11:09:48 +0800

    Update views.py
    delete comments

--- likes/views.py
+++ likes/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-
 from .models import Likes
 from django.contrib.contenttypes.models import ContentType
 import datetime
@@ -16,7 +15,7 @@ def Postlikes(request,post_id,like_type):
         like_type = False
 
     if user.is_authenticated:
-        hasbeen_liked = Likes.objects.filter(        # 这里必须用filter来实现，get，filter区别
+        hasbeen_liked = Likes.objects.filter(
             user=user,
             content_type=post_type,
             object_id=post_id,
