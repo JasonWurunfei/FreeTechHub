09e1317ab13c88ab2bcd5069e23f861db827b098
JasonWu <47469275+JasonWurunfei@users.noreply.github.com>
2020-06-29 14:26:16 +0800

    fix ui

--- QA/admin.py
+++ QA/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Rewarded_question)
+admin.site.register(Rewarded_answer)
+admin.site.register(Answer)
+admin.site.register(Question)
